66986ad90227643759eef668076b9f4b0ad8c4b0
d4r3devil <48920044+d4r3devil@users.noreply.github.com>
2021-05-12 11:14:04 +0530

    Update dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -1,5 +1,5 @@
 import os
 os.system('sudo apt install python3-pip')
-os.system('pip install selenium')
-os.system('pip install tkinter')
-os.system('pip install pynput')
+os.system('pip3 install selenium')
+os.system('pip3 install tkinter')
+os.system('pip3 install pynput')
